9366f1a45e587c1bae35bcf410c892377d9b3843
MarcDevGuy127 <marceloh.github@gmail.com>
2025-09-21 21:03:04 -0300

    Add first PHP files from the first class

---
+++ aula/ola.php
@@ -0,0 +1,27 @@
+ <?php
+ //echo "Olá, mundo!";
+
+    echo "Olá, mundo!";
+    print "<br>Usando PHP!";
+    /*$mensagem = "Bem-vindo ao PHP!";
+    $ano = 2025;
+    $versao = 8.3;
+    $ativo = true;
+
+    $nome = "João";
+    echo "Olá, $nome!";
+    echo "2 + 2 é " . (2 + 2);*/
+
+    $nome = "Marcelo";
+    
+    echo "<br>Olá, $nome!";
+
+    $x = 9;
+    $y = 7;
+
+    echo "<br>9 + 7 é " . ($x + $y);
+    echo "<br>9 - 7 é " . ($x - $y);
+    echo "<br>9 * 7 é " . ($x * $y);
+    echo "<br>9 / 7 é " . ($x / $y);
+
+ ?>
