1e4e5f39a0d83ce74cbcee14b298af5776e0d599
MarcDevGuy127 <marceloh.github@gmail.com>
2025-10-22 19:54:12 -0300

    Configuring app.php.

---
+++ ProgWEB/config/app.php
@@ -0,0 +1,13 @@
+<?php
+// config/app.php
+
+return [
+    'database' => [
+        'host' => 'localhost', // Ou o nome do servidor SQL Server
+        'database_name' => 'SistemaWebCRUD',
+        'user' => 'sa', // Usuário do SQL Server (geralmente 'sa' para Express)
+        'password' => 'Passw0rd', // A senha que você definiu para o usuário 'sa'
+    ]
+];
+
+?>;
